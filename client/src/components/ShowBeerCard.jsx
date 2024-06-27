44fe71a43b3cf147b70bd4509f44ab24628cfca4
jazasy <janoserdosy1@gmail.com>
2024-06-27 18:53:05 +0200

    started to make styles for description

--- client/src/components/ShowBeerCard.jsx
+++ client/src/components/ShowBeerCard.jsx
@@ -1,17 +1,41 @@
-import { Card, Typography, CardMedia, Rating, Box } from "@mui/material";
-import { showBeerCardStyle} from "../styles";
+import {
+	Card,
+	Typography,
+	CardMedia,
+	Rating,
+	Box,
+	CardContent,
+	Grid,
+} from "@mui/material";
+import { showBeerCardStyle } from "../styles";
 import ShowCardHeadBox from "./ShowCardHeadBox";
 
 export default function ShowBeerCard({ beer }) {
 	return (
 		<Card sx={showBeerCardStyle}>
 			<ShowCardHeadBox beer={beer} />
-			<CardMedia
-				sx={{ px: { xs: 3, md: "30%" } }}
-				component="img"
-				image={beer.image}
-			/>
-			<Typography variant="h4" align="center"></Typography>
+			<Grid container>
+				<Grid item xs={6}>
+						<CardContent>
+							<Typography variant="body1">
+								Az ország egyik kedvenc sörét, a Borsodi világost 1973 óta
+								folyamatosan tökéletesítjük fogyasztóinknak. Így a kiváló
+								minőségű, láger típusú sörünket mára tisztán árpamalátából,
+								kukoricadara nélkül főzzük. A Borsodi Világos íze az évek során
+								még kiegyensúlyozottabb lett, de továbbra is jellemző rá a
+								kellemes keserűség és a csillogó aranysárga szín, melyet dús,
+								finom pórusú hab koronáz.
+							</Typography>
+						</CardContent>
+				</Grid>
+				<Grid item xs={6}>
+					<CardMedia
+						/* sx={{ px: { xs: 3, md: "30%" } }} */
+						component="img"
+						image={beer.image}
+					/>
+				</Grid>
+			</Grid>
 		</Card>
 	);
 }
