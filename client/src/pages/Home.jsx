eb08c6f88f466feefd6268d488392f9ad4359d9d
jazasy <janoserdosy1@gmail.com>
2024-06-30 22:59:28 +0200

    made a basic sorting based on ratings

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 	const [data, setData] = useState([]);
 
 	const sortBeers = (beers) => {
-		
+		return beers.sort((a, b) => b.rating - a.rating)
 	}
 
 	useEffect(() => {
@@ -24,7 +24,7 @@ export default function Home() {
 				{data.length === 0 ? (
 					<LoadingText />
 				) : (
-					data.map((beer, i) => <HomeBeerCard beer={beer} key={beer._id} />)
+					sortBeers(data).map((beer, i) => <HomeBeerCard beer={beer} key={beer._id} />)
 				)}
 			</Container>
 		</>
